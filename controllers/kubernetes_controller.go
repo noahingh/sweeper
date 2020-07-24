ea69f61a280327435b7bad47567015d8f3f4f229
hanjunlee <opsdownn@gmail.com>
2020-07-25 05:28:45 +0900

    refactoring to run workers.

--- controllers/kubernetes_controller.go
+++ controllers/kubernetes_controller.go
@@ -18,6 +18,7 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	"strconv"
 	"sync"
 	"time"
@@ -27,6 +28,7 @@ import (
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/util/workqueue"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
@@ -43,6 +45,8 @@ type KubernetesReconciler struct {
 	client.Client
 	Log    logr.Logger
 	Scheme *runtime.Scheme
+
+	Workqueue workqueue.RateLimitingInterface
 }
 
 // +kubebuilder:rbac:groups=selector.sweeper.io,resources=kubernetes,verbs=get;list;watch;create;update;patch;delete
@@ -68,14 +72,28 @@ func (r *KubernetesReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) 
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
 
-	logger.Info("start to delete selected resources over TTL")
-	r.deleteResourcesOverTTL(ctx, ks.GetNamespace(), ks.Spec.GroupVersionKinds, ks.Spec.ObjectLabels, ks.Spec.TTL)
+	ttl, err := time.ParseDuration(ks.Spec.TTL)
+	if err != nil {
+		logger.Error(err, "failed to parse the duration")
+		return ctrl.Result{}, nil
+	}
 
-	next := r.getNextRun(ctx, ks.GetNamespace(), ks.Spec.GroupVersionKinds, ks.Spec.ObjectLabels, ks.Spec.TTL)
+	logger.Info("gather all items of GVK.")
+	resources := r.getGVKResources(ctx, ks.GetNamespace(), ks.Spec.GroupVersionKinds, ks.Spec.ObjectLabels)
 
-	logger.Info("wait to the next run", 
+	for _, resource := range resources {
+		_, err := r.enqueueResourcesOverTTL(ctx, resource, ttl)
+		if err != nil {
+			logger.Error(err, "failed to enqueue the resource")
+			continue
+		}
+	}
+
+	next := r.getNextRunTime(ctx, resources, ttl)
+	logger.Info("wait to the next run",
 		"wait(s)", next.Sub(time.Now()).Seconds(),
 	)
+
 	return ctrl.Result{
 		RequeueAfter: next.Sub(time.Now()),
 	}, nil
@@ -83,6 +101,15 @@ func (r *KubernetesReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) 
 
 // SetupWithManager -
 func (r *KubernetesReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	const (
+		thread = 2
+	)
+
+	// run workers
+	for i := 0; i < thread; i++ {
+		go r.runWorker()
+	}
+
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&selectorv1alpha1.Kubernetes{}).
 		Complete(r)
@@ -110,6 +137,89 @@ func (r *KubernetesReconciler) isEnabledNamespace(ctx context.Context, namespace
 	}
 
 	return isEnabled, nil
+}
+
+func (r *KubernetesReconciler) getGVKResources(
+	ctx context.Context,
+	namespace string,
+	gvks []selectorv1alpha1.GroupVersionKind,
+	labels map[string]string,
+) []unstructured.Unstructured {
+	var (
+		logger = r.Log
+
+		wg    = sync.WaitGroup{}
+		mutex = sync.Mutex{}
+		ret   = make([]unstructured.Unstructured, 0)
+	)
+
+	for _, gvk := range gvks {
+		wg.Add(1)
+
+		go func(group, version, kind string) {
+			defer wg.Done()
+
+			u := unstructured.UnstructuredList{}
+			u.SetGroupVersionKind(schema.GroupVersionKind{
+				Group:   group,
+				Version: version,
+				Kind:    kind,
+			})
+
+			if err := r.List(ctx, &u, client.InNamespace(namespace), client.MatchingLabels(labels)); err != nil {
+				logger.Error(err, "failed to list for gvk", "group", group, "version", version, "kind", kind)
+				return
+			}
+
+			// gathering all items of GVK.
+			for _, i := range u.Items {
+				mutex.Lock()
+				ret = append(ret, i)
+				mutex.Unlock()
+			}
+		}(gvk.Group, gvk.Version, gvk.Kind)
+	}
+
+	wg.Wait()
+
+	return ret
+}
+
+func (r *KubernetesReconciler) enqueueResourcesOverTTL(
+	ctx context.Context,
+	obj unstructured.Unstructured,
+	ttl time.Duration,
+) (bool, error) {
+	createdAt := obj.GetCreationTimestamp()
+	expiredAt := createdAt.Add(ttl)
+
+	if expiredAt.Before(time.Now()) {
+		r.Workqueue.Add(obj)
+		return true, nil
+	}
+
+	return false, nil
+}
+
+func (r *KubernetesReconciler) getNextRunTime(
+	ctx context.Context,
+	objs []unstructured.Unstructured,
+	ttl time.Duration,
+) time.Time {
+	var (
+		nextRun = time.Now().Add(time.Minute)
+	)
+
+	for _, obj := range objs {
+		createdAt := obj.GetCreationTimestamp()
+		expiredAt := createdAt.Add(ttl)
+
+		if expiredAt.Before(nextRun) {
+			nextRun = expiredAt
+		}
+	}
+
+	return nextRun
 }
 
 func (r *KubernetesReconciler) deleteResourcesOverTTL(
@@ -228,4 +338,48 @@ func (r *KubernetesReconciler) getNextRun(
 
 	wg.Wait()
 	return nextRun
+}
+
+// run workers to delete resources which is over TTL.
+func (r *KubernetesReconciler) runWorker() error {
+	for r.processNextWorkItem() {
+	}
+	return nil
+}
+
+func (r *KubernetesReconciler) processNextWorkItem() bool {
+	logger := r.Log
+
+	item, shutdown := r.Workqueue.Get()
+	if shutdown {
+		logger.Info("the workqueue is shutdown.")
+		return false
+	}
+
+	err := func(item interface{}) error {
+		defer r.Workqueue.Done(item)
+
+		obj, ok := item.(unstructured.Unstructured)
+		if !ok {
+			r.Workqueue.Forget(item)
+			return fmt.Errorf("failed to convert type")
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+
+		if err := r.Delete(ctx, &obj); err != nil {
+			r.Workqueue.Add(obj)
+			return fmt.Errorf("failed to delete the item: %s", obj.GetName())
+		}
+		logger.Info("delete the object: %s", obj.GetName())
+
+		return nil
+	}(item)
+	if err != nil {
+		logger.Error(err, "failed to process the item")
+		return true
+	}
+
+	return true
 }
